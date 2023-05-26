676233dd301e0e6dc3abe4fa1d4a287a36c8cf10
Umang Gohel <gohelumang12@gmail.com>
2023-05-26 12:02:10 +0530

    Use of @ViewChild

--- src/app/auth-form/auth-form.component.ts
+++ src/app/auth-form/auth-form.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Component, ContentChild, EventEmitter, Output } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ContentChild, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 import { AuthRememberComponent } from '../auth-remember/auth-remember.component';
 
 @Component({
@@ -6,20 +6,28 @@ import { AuthRememberComponent } from '../auth-remember/auth-remember.component'
   templateUrl: './auth-form.component.html',
   styleUrls: ['./auth-form.component.scss']
 })
-export class AuthFormComponent implements AfterContentInit{
+export class AuthFormComponent implements AfterContentInit,AfterViewInit{
+  
   
   name!:string;
   password!:string;
   showMessage:boolean=false;
   @ContentChild(AuthRememberComponent)
+  @Output()
+  submitted:EventEmitter<any>=new EventEmitter();
   remember!:AuthRememberComponent;
+  @ViewChild('email')
+  email!:ElementRef;
+
   ngAfterContentInit(): void {
     if(this.remember){
       this.remember.checked.subscribe((checked:boolean)=>this.showMessage=checked);
     }
   }
-  @Output()
-  submitted:EventEmitter<any>=new EventEmitter();
+  ngAfterViewInit(): void {
+    this.email.nativeElement.setAttribute("placeholder","Email");
+    this.email.nativeElement.focus();
+  }
   onSubmit(values:any){
     this.submitted.emit(values);
   }
